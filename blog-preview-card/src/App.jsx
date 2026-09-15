4dbdcf2ad99b4e1ca0fa45f9d76782108a0e7134
Lavinia Antunes Silva <lavinia.antunes@aluno.ifsp.edu.br>
2026-09-15 08:39:42 -0300

    Configurando layout

--- blog-preview-card/src/App.jsx
+++ blog-preview-card/src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <>
-      <h1 className="font-figtree text-3xl font-extrabold text-yellow-500">Hello, World!</h1>
-    </>
-  )
-}
+import { BrowserRouter, Routes, Route } from "react-router";
+import CenteredLayout from "./layouts/CenteredLayout";
+import HomePage from "./pages/HomePage";
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route element={<CenteredLayout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
+  );
+}
