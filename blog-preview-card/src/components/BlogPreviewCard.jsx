6d2ce14e5007a1d8375408f39608e1c2e3a3d561
Lavinia Antunes Silva <lavinia.antunes@aluno.ifsp.edu.br>
2026-09-22 08:25:02 -0300

    site terminado

---
+++ blog-preview-card/src/components/BlogPreviewCard.jsx
@@ -0,0 +1,43 @@
+import { formatDate } from "../utils/formatDate";
+
+export default function BlogPreviewCard({ post }) {
+    const { coverImage, categories, publishedAt, title, excerpt, author } =
+        post;
+    return (
+        <article className="border-gray-950 bg-white flex w-full max-w-96 flex-col gap-6 rounded-[20px] border p-6 shadow-[8px_8px_0_var(--color-gray950)] transition-shadow hover:shadow-[16px_16px_0_var(--color-gray-950)]">
+            <img src={coverImage} alt={title} className="w-full rounded-[10px]"
+            />
+            <div className="flex flex-col items-start gap-3">
+                <ul className="flex flex-wrap gap-2">
+                    {categories.map((category) => (
+                        <li
+                            key={category}
+                            className="bg-yellow text-gray-950 rounded px-3 py-1 text-sm
+font-extrabold"
+                        >
+                            {category}
+                        </li>
+                    ))}
+                </ul>
+                <p className="text-gray-950 text-sm font-medium">
+                    Published {formatDate(publishedAt)}
+                </p>
+                <h1 className="text-gray-950 hover:text-yellow cursor-pointer text2xl font-extrabold transition-colors">
+                    {title}
+                    11.4 APágina
+                    38
+                </h1>
+                <p className="text-gray-500 font-medium">{excerpt}</p>
+            </div>
+            <div className="flex items-center gap-3">
+                <img
+                    src={author.avatar}
+                    alt={author.name}
+                    className="size-8 rounded-full"
+                />
+                <p className="text-gray-950 text-sm font-extrabold">{author.name}
+                </p>
+            </div>
+        </article>
+    );
+}
---
+++ blog-preview-card/src/data/post.js
@@ -0,0 +1,13 @@
+export const post = {
+    id: 1,
+    coverImage: "/posts/banner-palestra.jpg",
+    categories: ["Learning"],
+    publishedAt: "2023-12-21",
+    title: "HTML & CSS foundations",
+    excerpt:
+"These languages are the backbone of every website, defining structure, content, and presentation.",
+    author: {
+        name: "Kaung Minn",
+        avatar: "/users/kaung-minn.jpg",
+    }
+};
